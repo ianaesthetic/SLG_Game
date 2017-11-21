34e7fb6abc893100387fce0a8796817ecc27e8b7
ianaesthetic <ianaesthetic@live.com>
2017-11-21 17:00:11 +0800

    。。。

--- Assets/Script/PathFinding.cs
+++ Assets/Script/PathFinding.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class heap { 
 	private List<int> id; 
-	private List<int> key; 
+	private List<int> keyReal;
+	private List<int> keyEstimate;
 	private List<int> dist; 
 	private List<int> l; 
 	private List<int> r; 
@@ -12,9 +13,10 @@ public class heap {
 	int nodeCount; 
 	int root; 
 
-	heap() {
+	public heap() {
 		id = new List<int> (); 
-		key = new List<int> (); 
+		keyReal = new List<int> (); 
+		keyEstimate = new List<int> (); 
 		dist = new List<int> (); 
 		l = new List<int> (); 
 		r = new List<int> ();
@@ -24,7 +26,8 @@ public class heap {
 
 	public void Clear() {
 		id.Clear (); 
-		key.Clear (); 
+		keyReal.Clear ();
+		keyEstimate.Clear ();
 		dist.Clear (); 
 		l.Clear (); 
 		r.Clear (); 
@@ -41,7 +44,7 @@ public class heap {
 	private int merge(int a, int b) {	
 		if (a < 0 || b < 0)
 			return a < 0 ? b : a;
-		if (key[b] < key[a])
+		if (key.Estimate[b] < key[a])
 			Swap (ref a, ref b); 
 		r [a] = merge (r [a], b);  
 		if (l[a] < 0 || dist [l [a]] < dist [r [a]]) {
@@ -82,14 +85,20 @@ public class map {
 	public struct node {
 		public int id;	
 		public int dist; 
+		public int preID;
 
-		public node(int _id, int _dist) {
+		public node(int _id, int _dist, int _preID) {
 			id = _id; 
 			dist = _dist;
+			preID = _preID;
 		}
 	};	
 		
 	List<node> nodeMem;
+
+	public map() {
+		nodeMem = new List<node> ();
+	}
 
 	public void Clear() {
 		nodeMem.Clear (); 
@@ -103,8 +112,17 @@ public class map {
 		return false; 
 	}
 		
-	public void Insert(int id, int dist) {
-		nodeMem.Add (new node(id, dist));
+	public int GetDist(int id) {
+		for (int i = 0; i < nodeMem.Count; ++i) {
+			if (nodeMem [i].id == id) {
+				return nodeMem[i].dist;
+			}
+		}
+		return PathFinding.MAXDIST;
+	}
+
+	public void Insert(int id, int dist, int pre) {
+		nodeMem.Add (new node(id, dist, pre));
 	}
 		
 	public void Delete(int id) { 
@@ -117,48 +135,87 @@ public class map {
 
 };
 
-public class PathFinding : MonoBehaviour {
+public class PathFinding : MonoBehaviour { 
 
-	private struct pair{
-		public float h; 
-		public float w; 
-
-		pair(float _h, float _w) {
-			h = _h; 
-			w = _w; 
-		}
-	};
+	static int MAXDIST = 0x3f3f3f3f;
 
 	public float lB, rB, uB, dB; 
 	public int nB;
-	private float wB, hB, hStart, wStart, scaleFactor, interval;
 	public GameObject hexagon; 
 
+	private float wB, hB, hStart, wStart, scaleFactor, interval;
 	private heap unCover; 
 	private map covered; 
-	private pair[] direction; 
+	private int[] direction; 
 
-	public void GetPos(out float posH, out float posW, int id) {
-		int tmp; 
-		tmp = (int)(id / nB); 
-		posH = (float)tmp * interval + dB;
-		tmp = id % nB; 
-		posW = (float)tmp * interval + lB;
+	public int GetID(int posH, int posW, bool flag) {
+		if (flag == false) {
+			if (posH < 0 || posH > nB - 1 || posW < 0 || posW > nB - 1)
+				return -1;
+			else
+				return posH * nB + posW;
+		}
+		else {
+			if (posH < 0 || posH > nB - 2 || posW < 0 || posW > nB - 2)
+				return -1;
+			else
+				return posH * (nB - 1) + posW + nB * nB;
+		}
+	}
+		
+	private void SetDirection(int id) {
+
+		int th, tw, nxtID; 
+		th = id / nB;
+		tw = id % nB;
+
+		if (id < nB * nB) {
+			nxtID = GetID (th - 1, tw, false); direction [0] = nxtID == -1 ? nxtID : -1; 
+			nxtID = GetID (th + 1, tw, false); direction [1] = nxtID == -1 ? nxtID : -1; 
+			nxtID = GetID (th - 1, tw - 1, true); direction [2] = nxtID == -1 ? nxtID : -1; 
+			nxtID = GetID (th - 1, tw, true); direction [3] = nxtID == -1 ? nxtID : -1; 
+			nxtID = GetID (th, tw - 1, true); direction [4] = nxtID == -1 ? nxtID : -1;
+			nxtID = GetID (th, tw, true); direction [5] = nxtID == -1 ? nxtID : -1; 
+		}
+		else {
+			nxtID = GetID (th - 1, tw, true); direction [0] = nxtID == -1 ? nxtID : -1; 
+			nxtID = GetID (th + 1, tw, true); direction [1] = nxtID == -1 ? nxtID : -1; 
+			nxtID = GetID (th, tw, false); direction [2] = nxtID == -1 ? nxtID : -1; 
+			nxtID = GetID (th, tw + 1, false); direction [3] = nxtID == -1 ? nxtID : -1; 
+			nxtID = GetID (th + 1, tw, false); direction [4] = nxtID == -1 ? nxtID : -1; 
+			nxtID = GetID (th + 1, tw + 1, false); direction [5] = nxtID == -1 ? nxtID : -1; 
+		}
 	}
 
-	public void GetID(float posH, float posW, out int id) {
-		if (posH < hStart || posH > hStart + hB || posW < wStart || posW > wStart + wB) {
-			id = -1; 
-			return; 
+	public void GetPos(out float posH, out float posW, int id) {
+		if (id < nB * nB) {
+			int tmp; 
+			tmp = (int)(id / nB); 
+			posH = (float)tmp * interval + dB;
+			tmp = id % nB; 
+			posW = (float)tmp * interval + lB;
 		}
+		else {
+			id -= nB * nB;
+			int tmp; 
+			tmp = (int)(id / (nB - 1));
+			posH = ((float)tmp + 0.5f) * interval + dB; 
+			tmp = id % (nB - 1); 
+			posW = ((float)tmp + 0.5f) * interval + lB;
+		}
+	}
 
+	public int GridDistCost(int id) {
+		return 1; 
 	}
 
 	void Start() {
 
 		float ratio = Mathf.Sqrt (3); 
 		scaleFactor = 0.9f;
-
+		direction = new int[6]; 
+		unCover = new heap (); 
+		covered = new map ();
 
 		wB = rB - lB;
 		hB = wB - dB;
@@ -166,8 +223,7 @@ public class PathFinding : MonoBehaviour {
 		if (hB * ratio < wB) {
 			hB *= scaleFactor; 
 			wB = hB * ratio; 
-		}
-		else {
+		} else {
 			wB *= scaleFactor; 
 			hB = wB / ratio; 
 		}
@@ -176,22 +232,21 @@ public class PathFinding : MonoBehaviour {
 		hStart = dB + (wB - dB - hB) / 2.0f; 
 		wStart = lB + (rB - lB - wB) / 2.0f; 
 	
-		for (int i = 0; i < nB; ++i)
+		for (int i = 0; i < nB; ++i) {
 			for (int j = 0; j < nB; ++j) {
-				GameObject tObject = Instantiate (hexagon, new Vector3 (i * interval + hStart, j * interval + wStart), Quaternion.identity) as GameObject;
+				GameObject tObject = Instantiate (hexagon, new Vector3 (i * interval + hStart, j * interval + wStart, 0), Quaternion.identity) as GameObject;
 				tObject.GetComponent<Hexagon> ().SetId (i * nB + j);
 			}
+		}
 
-		float tInterval_0 = interval / 2.0; 
-		float tInterval_1 = tInterval_0 * ratio; 
-	
-		direction = new int[6] (); 
-		direction [0] = new pair (tInterval_0, tInterval_1);
-		direction [1] = new pair (interval, 0); 
-		direction [2] = new pair (tInterval_0, -tInterval_1); 
-		direction [3] = new pair (-tInterval_0, tInterval_1); 
-		direction [4] = new pair (-interval, 0); 
-		direction [5] = new pair (-tInterval_0, tInterval_1);
+		for (int i = 0; i < nB - 1; ++i)
+			for (int j = 0; j < nB - 1; ++j) {
+				GameObject tObject = Instantiate (
+					hexagon, 
+					new Vector3 (((float)i + 0.5f) * interval + hStart, ((float)j + 0.5f) * interval + wStart, 0),
+					Quaternion.identity) as GameObject;
+				tObject.GetComponent<Hexagon> ().SetId (i * (nB - 1) + j + nB * nB); 
+			}
 	}
 
 	public void FindPath(GameObject unit, int dest) {
@@ -201,9 +256,16 @@ public class PathFinding : MonoBehaviour {
 		covered.Clear (); 
 		unCover.Insert (move.curPos, 0);
 		int curID = unCover.TopID (); 
+		var curIDDist = unCover.TopKey();
 		while (curID != dest) {
-			covered.Insert (curID, unCover.TopKey ()); 
-			unCover.Pop (); 
+			covered.Insert (curID, unCover.TopKey (), -1); 
+
+			SetDirection (curID);
+			for (int i = 0; i < 6; ++i) {
+				if (direction [i] != -1 && covered.GetDist(direction[i]) > GridDistCost(direction[i]) +  curIDDist) {
+					
+				}
+			}
 		}
 	}
 }
